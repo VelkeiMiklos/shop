65f940eee92dc60d7c7d26f0a619df1ec185ea96
VelkeiMiklos <velkei18@gmail.com>
2017-10-16 17:34:20 +0200

    Initial Commit

---
+++ shop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  shop
+//
+//  Created by Velkei Miklós on 2017. 10. 16..
+//  Copyright © 2017. NeonatCore. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
